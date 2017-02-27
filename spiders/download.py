6123ccfd31192ab5053bba4df1899ec306fe85d0
Napster <txdyc@hotmail.com>
2017-02-27 16:07:01 +0800

    modify functions

--- spiders/download.py
+++ spiders/download.py
@@ -1,16 +1,22 @@
 # coding: utf-8
 
 import urllib2
+import urlparse
 from config import user_agents
 
 
-def download(url, user_agent='wswp', num_retries=2):
+def download(url, user_agent='wswp', proxy=None, num_retries=2):
     print 'Downloading: ', url
     headers = {'User-agents': user_agent}
     request = urllib2.Request(url, headers=headers)
+    opener = urllib2.build_opener()
+
+    if proxy:
+        proxy_params = {urlparse.urlparse(url).scheme: proxy}
+        opener.add_handler(urllib2.ProxyHandler(proxy_params))
 
     try:
-        html = urllib2.urlopen(request).read()
+        html = opener.urlopen(request).read()
     except urllib2.URLError as e:
         print 'Download error: ', e.reason
         html = None
@@ -18,6 +24,6 @@ def download(url, user_agent='wswp', num_retries=2):
         if num_retries > 0:
             if hasattr(e, 'code') and 500 <= e.code < 600:
                 # recursively retry 5xx HTTP errors
-                return download(url, user_agent, num_retries-1)
+                return download(url, user_agent, proxy, num_retries-1)
 
     return html
--- spiders/link_crawler.py
+++ spiders/link_crawler.py
@@ -2,19 +2,38 @@
 
 import re
 import download
+import urlparse
+import robotparser
+from libraries.throttle import *
 
 
-def link_crawler(seed_url, link_regex):
+def link_crawler(seed_url, link_regex, max_depth=2):
     """ Crawl from the given seed URL following links matched by link_regex """
-
+    max_depth = 2
     crawl_queue = [seed_url]
+    throttle = Throttle(delay)
+
+    # keep track which URL's have seen before
+    seen = set(crawl_queue)
     while crawl_queue:
         url = crawl_queue.pop()
-        html = download.download(url)
+        # check url passes robots.txt restriction
+        if check_robots(url, user_agent, robots_name='robots.txt'):
+            throttle.wait(url)
+            html = download.download(url, user_agent)
 
-        # filter for links matching our regular expression
-        for link in get_links(html):
-            crawl_queue.append(link)
+            # filter for links matching our regular expression
+            for link in get_links(html):
+                # check if link matches expected regex
+                if re.match(link_regex, link):
+                    # form absolute link
+                    link = urlparse.urljoin(seed_url, link)
+                    # check if have already seen thie link
+                    if link not in seen:
+                        seen.add(link)
+                        crawl_queue.append(link)
+        else:
+            print 'Blocked by robots.txt: ', url
 
 
 def get_links(html):
@@ -25,3 +44,12 @@ def get_links(html):
 
     # list of all links from the webpage
     return webpage_regex.findall(html)
+
+
+def check_robots(url, user_agent, robots_name='robots.txt'):
+    rp = robotparser.RobotFileParser()
+    rp.set_url(urlparse.urljoin(url, robots_name))
+    rp.read()
+    result = rp.can_fetch(user_agent, url)
+
+    return result
